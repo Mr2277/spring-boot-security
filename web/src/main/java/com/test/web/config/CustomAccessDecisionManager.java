a8208307a9a7554e5fecde26da48dbc8642af109
sh <837544713@qq.com>
2018-06-22 10:58:19 +0800

    自定义AccessDecitionManager  判断决定当前url拥有的角色是否符合当前登录的角色

--- web/src/main/java/com/test/web/config/CustomAccessDecisionManager.java
+++ web/src/main/java/com/test/web/config/CustomAccessDecisionManager.java
@@ -10,21 +10,24 @@ import org.springframework.security.core.GrantedAuthority;
 
 import java.util.Collection;
 import java.util.Iterator;
+/**
+ * 此类是决策器： 用来对 用户应有的角色,与URL地址可以访问的角色进行对比,如果不匹配则抛出异常
+ */
 
 public class CustomAccessDecisionManager implements AccessDecisionManager {
     private static final Logger logger = Logger.getLogger(CustomAccessDecisionManager.class);
-
+    /**
+     * 决策方法： 如果方法执行完毕没有抛出异常,则说明可以放行, 否则抛出异常 AccessDeniedException
+     */
     @Override
-    public void decide(Authentication authentication, Object object,
-                       Collection<ConfigAttribute> configAttributes)
-            throws AccessDeniedException, InsufficientAuthenticationException {
+    public void decide(Authentication authentication, Object object,Collection<ConfigAttribute> configAttributes) throws AccessDeniedException, InsufficientAuthenticationException {
         if (configAttributes == null) {
             return;
         }
-
         //config urlroles
         Iterator<ConfigAttribute> iterator = configAttributes.iterator();
-
+        // 如果登陆成功,则信息会存储在Authorities中
+       // 前台传入的url的角色，UrlDaoImpl.getAttributes获得的
         while (iterator.hasNext()) {
             ConfigAttribute configAttribute = iterator.next();
             //need role
